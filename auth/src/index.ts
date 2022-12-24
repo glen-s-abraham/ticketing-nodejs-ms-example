25435865495280d30984fbc0b13dfca722faaaca
glen-s-abraham <glenprojects666@gmail.com>
2022-12-24 20:58:56 +0530

    proper error handlers added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,25 @@
 import express from "express";
+import 'express-async-errors'
 import {json} from 'body-parser';
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
 app.use(json());
 
-app.get('/api/users/currentuser',(req,res)=>{
-    res.send('current user');
-})
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+app.all('*',async (req,res,next)=>{
+    throw new NotFoundError()
+});
+app.use(errorHandler)
 
 
 app.listen(3000,()=>{
